0af7d77125783ce9b8d8dea4dc04982cfd270e75
am1re <am1re344@gmail.com>
2020-01-25 23:19:00 +0200

    Deezer player added

--- routes/textSearch.js
+++ routes/textSearch.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var request = require('request')
+const fetch = require('node-fetch');
 var config = require('../config');
 
 /* GET */
@@ -9,43 +9,31 @@ router.get('/', async function (req, res, next) {
   const lyrics = req.query.lyrics;
   if (!lyrics) return res.render('textSearch', { title: 'Text Search - The Milk' });
 
-  const data = {
+  const formData = {
     'return': 'timecode,apple_music,deezer,spotify',
     'api_token': config.audd_api_token,
     'method': 'findLyrics',
     'q': lyrics
   };
-  
-  request({
-    uri: 'https://api.audd.io/',
-    form: data,
-    method: 'POST'
-  }, async function (err, resApi, body) {
-    var data = JSON.parse(body);
 
-    // ссылки на песни не работают сейчас :(
-      
-    // console.log(data.result.length);
+  var results = await fetch("https://api.audd.io/", {body: JSON.stringify(formData), method: 'POST'});
+  var data = await results.json();
 
-    // await data.result.forEach(async (song, index, object) => {
-    //   const artist = song.artist;
-    //   const track = song.title;
-    //   await request(`https://api.deezer.com/search?q=artist:"${artist}"track:"${track}"`, function (err, resApi, body) {
-    //     var deezerData = "";
-    //     try {
-    //       deezerData = JSON.parse(body);
-    //     }
-    //     catch (e) { }
-
-    //     if(!deezerData.total || deezerData.total <= 0) data.result.splice(index, 1);
-    //   });
-    // });
-
-    // console.log(data.result.length);
-
-    res.render('textSearch', { title: 'Text Search - The Milk', data: data.result });
-  });
-
+  for(let i = 0; i < data.result.length; i++){
+    const artist = data.result[i].artist;
+    const track = data.result[i].title;
+    const response = await fetch(`https://api.deezer.com/search?q=artist:"${artist}"track:"${track}"`);
+    
+    var deezerData;
+    try {
+      deezerData = await response.json();
+    }
+    catch(e){}
+    
+    if(!deezerData || !deezerData.total || deezerData.total <= 0) data.result.splice(i, 1);
+    if(data.result[i] && deezerData) data.result[i].deezerData = deezerData.data;
+  }
+  res.render('textSearch', { title: 'Text Search - The Milk', data: data.result });
 });
 
 module.exports = router;
